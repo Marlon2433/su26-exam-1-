552a46535ca1eee2ce95bb29073ee2be3fad049f
Marlon2433 <164610133+Marlon2433@users.noreply.github.com>
2026-07-08 14:08:05 -0500

    edited files

--- sql.py
+++ sql.py
@@ -1,38 +1,38 @@
-import mysql.connector 
+import mysql.connector
 from mysql.connector import Error
 
-def DBConnection(hname,uname,passd,dbname):
-    con=None
+
+def DBConnection(hname, uname, passd, dbname):
+    con = None
     try:
-        con=mysql.connector.connect(
+        con = mysql.connector.connect(
             host=hname,
             user=uname,
             password=passd,
             database=dbname
         )
-        print("DB Connection successful")
-    except Error as e: 
+        print("DB connection successful")
+    except Error as e:
         print("Error is:", e)
-    return con 
+    return con
 
-def execute_read_query(con, query):
-    cursor=con.cursor(dictionary=True)
-    allrows=None
+
+def execute_read_query(con, query, params=None):
+    cursor = con.cursor(dictionary=True)
+    allrows = None
     try:
-        cursor.execute(query) 
-        allrows=cursor.fetchall()
+        cursor.execute(query, params or ())
+        allrows = cursor.fetchall()
         return allrows
     except Error as e:
-        print("Error is ", e)
+        print("Error is:", e)
 
-def execute_query(con, query):
-    cursor=con.cursor()
+
+def execute_query(con, query, params=None):
+    cursor = con.cursor()
     try:
-        cursor.execute(query) 
+        cursor.execute(query, params or ())
         con.commit()
         print("DB is updated")
     except Error as e:
-        print("Error is ", e)
-
-
-        
+        print("Error is:", e)
